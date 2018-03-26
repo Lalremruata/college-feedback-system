9852fcd394a3043cc6b239490c8ca28224dec7c2
Lalremruata <maruatajongte@gmail.com>
2018-03-26 19:03:01 +0530

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,5 @@
+@section('header')
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+@endsection
