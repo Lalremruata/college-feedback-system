56d8d080ecf234729e31e6fbe46c2bfaafd09053
Lalremruata <maruatajongte@gmail.com>
2018-04-03 10:25:39 +0530

    landing page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="btn btn-lg btn-success" style="margin:auto 15px"><a href="#" class="w3-text-black">Admin Login</a></li>
+                    <li class="btn btn-lg btn-success" style="margin:auto 15px"><a href="{{ route('login') }}" class="w3-text-black">Admin Login</a></li>
                 </ul>
             </div>
         </div>
@@ -22,7 +22,7 @@
                 <div class="row">
                         <div class="w3-animate-top w3-black" id="txt" style="height: 160px; width: 160px; font-size: 27px; padding: 55px 0px 0 0; border-radius: 50%; margin: 0 auto; border:8px solid #5a738e"></div>
                         <div class=" w3-animate-top w3-round-large w3-center  " style="font-size: 27px;" id="date"></div>
-                        <div style="border-bottom: 1px dotted black"> </div>
+                        <div style="border-bottom: 1px dotted black; max-width: 330px;margin: 0 auto;"> </div>
                     </div>
                 </div>
             </div>
